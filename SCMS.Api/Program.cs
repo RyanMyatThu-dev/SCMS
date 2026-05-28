e906ecb63dbe37822872f42bf26f4358f2aaa20f
theinhtetaung-dev <theinhtetaung.developer@gmail.com>
2026-05-28 14:28:16 +0630

    fix: bugs

--- SCMS.Api/Program.cs
+++ SCMS.Api/Program.cs
@@ -60,6 +60,7 @@ builder.Services
             }
         };
     });
+
 builder.Services.AddAuthorization();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
